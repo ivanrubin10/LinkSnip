800d3795f0ced9bc5077ba544864fc25d5050569
ivanrubin10 <ivanrubin10@gmail.com>
2024-01-24 15:17:41 -0300

    (feature): added copy button and changed UI

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-gray-100">
-    <header class="bg-blue-500 text-white p-4 text-center">
-      <h1 class="text-3xl font-bold">Link Shortener</h1>
+  <div class="flex flex-col min-h-screen bg-gray-100 bg-slate-500">
+    <header class="bg-blue-500 text-white p-4">
+      <h1 class="text-3xl font-bold w-3/4 mx-auto">Link Shortener</h1>
     </header>
-    <main class="flex-1 p-4 flex flex-col md:flex-row">
+    <main class="flex-1 p-4 flex flex-col md:flex-row mx-auto w-3/4 bg-slate-100">
       <section class="shorten-section flex-1 md:mr-4 mb-4 md:mb-0">
         <h2 class="text-lg font-semibold mb-4 text-gray-800">Shorten your link</h2>
-        <form @submit.prevent="shortenLink" class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-2">
+        <form @submit.prevent="shortenLink" class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:w-3/5">
           <label for="originalLink" class="sr-only">Original Link:</label>
           <input type="url" id="originalLink" v-model="originalLink" required placeholder="Enter your link" class="flex-1 p-3 border border-blue-500 rounded focus:outline-none focus:border-blue-700" />
-          <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Shorten</button>
+          <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700 ml-2">Shorten</button>
         </form>
       </section>
 
-      <aside class="history-sidebar w-full md:w-1/4 p-4 bg-gray-200">
+      <aside class="history-sidebar w-full md:w-2/5 p-4 bg-gray-200">
         <h2 class="text-lg font-semibold mb-4 text-gray-800">Shortened URLs History</h2>
         <ul>
-          <li v-for="(shortLink, index) in shortenedLinks" :key="index" class="text-blue-500 hover:underline mb-2">
-            <a :href="shortLink.shortened" target="_blank">{{ shortLink.shortened }}</a>
+          <li v-for="(shortLink, index) in shortenedLinks" :key="index" class="mb-2">
+            <div class="flex items-center space-x-2">
+              <a :href="shortLink.shortened" target="_blank" class="text-blue-500 hover:underline">{{ shortLink.shortened }}</a>
+              <button @click="copyToClipboard(shortLink.shortened)" class="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Copy</button>
+              <p class="text-gray-600">{{ shortLink.original.substring(0, 35) + '...' }}</p>
+            </div>
           </li>
         </ul>
       </aside>
@@ -73,6 +77,16 @@ async function shortenLink(): Promise<void> {
       console.log(error.message);
     }
   }
+}
+function copyToClipboard(link: string): void {
+  const textArea = document.createElement('textarea');
+  textArea.value = link;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+
+  console.log('Link copied to clipboard');
 }
 </script>
 
