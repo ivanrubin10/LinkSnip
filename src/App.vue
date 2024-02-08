6cb82a79fa062dc2c38877eb13f99c872e7c979e
ivanrubin10 <ivanrubin10@gmail.com>
2024-02-07 23:30:59 -0300

    (fix): input text contrast

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
     </header>
     <main class="flex-1 p-4 flex flex-col md:flex-row mx-auto w-3/4 bg-slate-100">
       <section class="shorten-section flex-1 md:mr-4 mb-4 md:mb-0">
-        <h2 class="text-lg font-semibold mb-4 text-gray-800">Shorten your link</h2>
+        <h2 class="text-lg font-semibold mb-4 text-gray-800">Shorten your link!</h2>
         <form @submit.prevent="shortenLink" class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:w-3/5">
           <label for="originalLink" class="sr-only">Original Link:</label>
-          <input type="url" id="originalLink" v-model="originalLink" required placeholder="Enter your link" class="flex-1 p-3 border border-blue-500 rounded focus:outline-none focus:border-blue-700" />
+          <input type="url" id="originalLink" v-model="originalLink" required placeholder="Enter your link" class="flex-1 p-3 border border-blue-500 rounded focus:outline-none focus:border-blue-700 text-gray-400" />
           <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700 ml-2">Shorten</button>
         </form>
       </section>
@@ -112,11 +112,5 @@ function removeLink(index: number): void {
   shortenedLinks.value.splice(index, 1);
 }
 </script>
-
-
-
-
-
-
 
 <style></style>
